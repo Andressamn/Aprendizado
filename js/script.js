7c1b8084a36d85c1d4bf066bb6323af66acdf7d2
Andressamn <andressamenegon@icloud.com>
2025-02-05 19:47:17 -0300

    atualização

--- js/script.js
+++ js/script.js
@@ -1,28 +1,33 @@
+document.getElementById('iniciarJogo').addEventListener('click', function() {
 alert('Boas vindas ao jogo do número secreto');
-let NumeroSecreto = 4;
-console.log(NumeroSecreto)
-let chute 
-let tentativas = 1
+let numeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
 
 //enquanto chute não for igual ao n.s;
-while(chute != NumeroSecreto) {
-    chute = prompt('Escolha um número entre 1 e 10');
-}
+while(chute != numeroSecreto) {
+    chute = parseInt(prompt(`Escolha um número entre 1 e ${numeroMaximo}`));
 
 //Se chute for igual ao número secreto;
-{if (chute == NumeroSecreto) {
+if (chute == numeroSecreto) {
     break;
 } else {
-    if (chute > NumeroSecreto) {
+    if (chute > numeroSecreto) {
     alert(`O número secreto é menor que ${chute}`);
-} else{
-    alert(`O número secreto é maior que ${chute}`)
+} else {
+    alert(`O número secreto é maior que ${chute}`);
 }} 
 //tentativas = tentativas + 1;
 tentativas++;
 }
-if (tentativas > 1){
-alert(`Isso aí, você descobriu o ${NumeroSecreto} com ${tentativas} tentativas`);
-} else {
-    alert(`Isso aí, você descobriu o ${NumeroSecreto} com ${tentativas} tentativa`);
-}
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso aí, você descobriu o ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
+});
+
+//if (tentativas > 1){
+//alert(`Isso aí, você descobriu o ${NumeroSecreto} com ${tentativas} tentativas`);
+//} else {
+ //   alert(`Isso aí, você descobriu o ${NumeroSecreto} com ${tentativas} tentativa`);
+//}
